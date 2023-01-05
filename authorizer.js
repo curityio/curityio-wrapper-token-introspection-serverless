21d1b0b3b1bc3e468043b95cfbbf0f0b436c23e2
Surendra Khatana <surendra.khatana@curity.io>
2023-01-05 08:56:33 +0530

    initial commit

--- authorizer.js
+++ authorizer.js
@@ -1,3 +1,19 @@
+/*
+ *  Copyright 2023 Curity AB
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
 const jose = require('jose');
 const axios = require('axios');
 
@@ -63,6 +79,7 @@ exports.handler = async (event, context, callback) => {
     const JWKS = jose.createRemoteJWKSet(new URL(response.data.jwks_uri));
     await jose.jwtVerify(wrapperAccessToken, JWKS, {});
     console.debug('Authorizer: Wrapper token validation is successful');
+
     // Return the Policy object
     callback(null, generatePolicy('userId', 'Allow', event.methodArn, issuer));
     console.debug('Authorizer: handler function completed');
@@ -72,13 +89,12 @@ exports.handler = async (event, context, callback) => {
       // that falls out of the range of 2xx
       console.error(error.response.data);
       console.error(error.response.status);
-      console.error(error.response.headers);
     } else if (error.request) {
       // The request was made but no response was received
       console.error(error.request);
     } else {
       // Something happened in setting up the request that triggered an Error
-      console.error('Error : ', error.message);
+      console.error('Authorizer: Error : ', error.message);
     }
     console.error(error.config);
     callback(null, generatePolicy('userId', 'Deny', event.methodArn));
--- handler.js
+++ handler.js
@@ -1,19 +1,34 @@
+/*
+ *  Copyright 2023 Curity AB
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
 const axios = require('axios');
 
 module.exports.introspectWrapperTokens = async (event, context, callback) => {
-  console.log('Handler: introspectWrapperTokens invoked');
-  // fetch issuer from the context
+  console.debug('Handler: introspectWrapperTokens invoked');
+  // get issuer from the context
   const issuer = event.requestContext.authorizer.issuer;
-  console.log(`Handler: Issuer: ${issuer}`);
+  console.debug(`Handler: Issuer: ${issuer}`);
 
   try {
-    // fetch token endpoint
+    // get token introspection endpoint
     const response = await axios.get(`${issuer}/.well-known/openid-configuration`);
     console.debug(`Handler: Token Introspection endpoint: ${response.data.introspection_endpoint}`);
 
-    const basic_auth_header = Buffer.from(`${process.env.OAUTH2_INTROSPECTION_CLIENT_ID.trim()}:${process.env.OAUTH2_INTROSPECTION_CLIENT_SECRET.trim()}`, 'utf-8').toString(
-      'base64'
-    );
+    //Base64 encode client_id and client_secret to authenticate to token introspection endpoint
+    const basic_auth_header = Buffer.from(`${process.env.CLIENT_ID.trim()}:${process.env.CLIENT_SECRET.trim()}`, 'utf-8').toString('base64');
 
     const requestData = {
       token: event.headers.Authorization
@@ -27,16 +42,16 @@ module.exports.introspectWrapperTokens = async (event, context, callback) => {
       }
     };
 
-    // call introspection method
-    const IntrospectedJWTResponse = await axios.post(response.data.introspection_endpoint, requestData, options);
-    console.log('Handler: introspectWrapperTokens completed');
+    // call introspection endpoint
+    const introspectionResponse = await axios.post(response.data.introspection_endpoint, requestData, options);
+    console.debug('Handler: introspectWrapperTokens completed');
 
     // return introspected JWT
     return {
       statusCode: 200,
       body: JSON.stringify(
         {
-          token: IntrospectedJWTResponse.data
+          token: introspectionResponse.data
         },
         null,
         2
@@ -48,13 +63,12 @@ module.exports.introspectWrapperTokens = async (event, context, callback) => {
       // that falls out of the range of 2xx
       console.error(error.response.data);
       console.error(error.response.status);
-      console.error(error.response.headers);
     } else if (error.request) {
       // The request was made but no response was received
       console.error(error.request);
     } else {
       // Something happened in setting up the request that triggered an Error
-      console.error('Error : ', error.message);
+      console.error('Handler: Error : ', error.message);
     }
     console.error(error.config);
   }
